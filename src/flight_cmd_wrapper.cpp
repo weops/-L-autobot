fc6f5890ab498a949531b6c35db8fa8fd42fbe01
Calvin Ngan <calvinthomasonngan@gmail.com>
2014-11-15 03:51:04 -0500

    added flight_cmd_wrapper cpp place holder and added launch file

---
+++ src/flight_cmd_wrapper.cpp
@@ -0,0 +1,26 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <sstream>
+
+int main( int argc, char **argv)
+{
+    ros::init( argc, argv, "flight_cmd_wrapper" );
+    
+    ros::NodeHandle n;
+
+    ros::Rate loop_rate( 10 );
+
+    ROS_INFO( "flight_cmd_wrapper start" );
+    while (ros::ok())
+    {
+        // your code
+        //ROS_INFO( "testing" );
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+
+    return 0;
+}
+
