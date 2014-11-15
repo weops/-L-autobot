dd20cfd600cfda37be61eb670a829f4af8569f38
Calvin Ngan <calvinthomasonngan@gmail.com>
2014-11-15 07:10:53 -0500

    add header files for GPIO

---
+++ include/we_autobot/GPIOExample.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <we_autobot/GPIOClass.h>
+
+#include "ros/ros.h"
+//int testGPIO()(int inpin, int outpin);
