23a4b6a8ed0098cb5899a5c43b6b1b942fff39f6
Calvin Ngan <calvinthomasonngan@gmail.com>
2014-11-15 03:26:51 -0500

    updated description of flight control

--- src/flight_control.cpp
+++ src/flight_control.cpp
@@ -3,6 +3,12 @@
 
 #include <sstream>
 
+/*
+ * flight algo that calculate flight path from reading sensor data and determining distance and direction of its target
+ * move the drone by calling service from flight_cmd_wrapper
+ *
+ */
+
 int main( int argc, char **argv)
 {
     ros::init( argc, argv, "flight_control" );
@@ -11,10 +17,11 @@ int main( int argc, char **argv)
 
     ros::Rate loop_rate( 10 );
 
+    ROS_INFO( "flight_control start" );
     while (ros::ok())
     {
         // your code
-        ROS_INFO( "testing" );
+        //ROS_INFO( "testing" );
     }
 
     ros::spinOnce();
