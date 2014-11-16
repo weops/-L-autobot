96d9672d87ba41fe385d0c9f35a462c5dee3708b
Calvin Ngan <calvinthomasonngan@gmail.com>
2014-11-16 09:38:10 -0500

    split flight contorl into interactive and auto mode , param is set thru lauch file

--- src/flight_control.cpp
+++ src/flight_control.cpp
@@ -5,98 +5,62 @@
  */
 
 #include <we_autobot/flight_control.h>
+bool flight_interactive( FlightCmdWrapper *flightCmdWrapper );
+bool flight_auto( FlightCmdWrapper *flightCmdWrapper );
+
+enum DroneCMD
+{
+   LED,
+   FLAT_TRIM,
+   LAUNCH,
+   LAND,
+   ADVANCE,
+   STRIDE,
+   UP,
+   TURN,
+   HOVER,
+   EXIT
+};
+
 
 int main( int argc, char **argv)
 {
   bool  programExit = false;
-
-  enum DroneCMD
-  {
-     LED,
-     FLAT_TRIM,
-     LAUNCH,
-     LAND,
-     ADVANCE,
-     STRIDE,
-     UP,
-     TURN,
-     HOVER,
-     EXIT
-  };
-
-  DroneCMD cmd;
-  int       inputCMD;
+  bool  controlMode = false; // false: interactive CMD line, true: auto
 
   ros::init( argc, argv, "flight_control" );
   
-  ros::NodeHandle n;
+  ros::NodeHandle n("~");
 
   ros::Rate loop_rate( 10 );
 
-  //FlightCmdWrapper flightCmdWrapper(TTL);
-  FlightCmdWrapper flightCmdWrapper(WIFI);
+  n.getParam( "controlMode", controlMode );
+  ROS_INFO( "control mode is %d", controlMode );
 
+  //FlightCmdWrapper flightCmdWrapper(TTL);
+  FlightCmdWrapper *flightCmdWrapper = new FlightCmdWrapper(WIFI);
 
   // TODO: parse in from rosrun arg for program mode: CMD line or Auto
   //
   while (!ros::ok());
-  while (!flightCmdWrapper.check_drone_ready());
-  flightCmdWrapper.print_connection_mode();
+  //system( "rosrun ardrone_autonomy ardrone_driver" );
+  ROS_INFO( "waiting for drone..." );
+  while (!flightCmdWrapper->check_drone_ready());
+  flightCmdWrapper->print_connection_mode();
   ROS_INFO( "flight_control start" );
 
   // wait till drone is ready
 
   while (ros::ok() && !programExit)
   {
-    cout  << LED        << ": led\n"
-          << FLAT_TRIM  << ": flat_trim\n"
-          << LAUNCH     << ": launch\n"
-          << LAND       << ": land\n"
-          << ADVANCE    << ": advance\n"
-          << STRIDE     << ": stride\n"
-          << UP         << ": up\n"
-          << TURN       << ": turn\n"
-          << HOVER      << ": hover\n"
-          << EXIT       << ": exit\n";
-    cin >> inputCMD;
-    cout << "I received:" << inputCMD << endl;
-    cmd = (DroneCMD)inputCMD;
-    switch (cmd)
+    if (controlMode)
     {
-      case LED:
-        flightCmdWrapper.led_animation();
-        break;
-      case FLAT_TRIM:
-        flightCmdWrapper.flat_trim();
-        break;
-      case LAUNCH:
-        flightCmdWrapper.flight_launch();
-        break;
-      case LAND:
-        flightCmdWrapper.flight_land();
-        break;
-      case ADVANCE:
-        flightCmdWrapper.flight_advance( 0.2 );
-        break;
-      case STRIDE:
-        flightCmdWrapper.flight_stride( 0.2 );
-        break;
-      case UP:
-        flightCmdWrapper.flight_up( 0.2 );
-        break;
-      case TURN:
-        flightCmdWrapper.flight_turn( -0.2 );
-        break;
-      case HOVER:
-        flightCmdWrapper.flight_hover();
-        break;
-      case EXIT:
-        ROS_INFO( "Exiting programming" );
-        programExit = true;
-        break;
-      default:
-        ROS_INFO( "Wrong CMD" );
-        break;
+      programExit = flight_auto( flightCmdWrapper );
+    }
+    else
+    {
+
+      programExit = flight_interactive( flightCmdWrapper );
     }
   }
 
@@ -104,5 +68,73 @@ int main( int argc, char **argv)
   loop_rate.sleep();
 
   return 0;
+}
+
+// return true if flight is done
+bool flight_interactive( FlightCmdWrapper *flightCmdWrapper )
+{
+  DroneCMD  cmd;
+  int       inputCMD;
+  bool      programExit = false;
+
+  cout  << LED        << ": led\n"
+        << FLAT_TRIM  << ": flat_trim\n"
+        << LAUNCH     << ": launch\n"
+        << LAND       << ": land\n"
+        << ADVANCE    << ": advance\n"
+        << STRIDE     << ": stride\n"
+        << UP         << ": up\n"
+        << TURN       << ": turn\n"
+        << HOVER      << ": hover\n"
+        << EXIT       << ": exit\n";
+  cin >> inputCMD;
+  cout << "I received:" << inputCMD << endl;
+  cmd = (DroneCMD)inputCMD;
+  switch (cmd)
+  {
+    case LED:
+      flightCmdWrapper->led_animation();
+      break;
+    case FLAT_TRIM:
+      flightCmdWrapper->flat_trim();
+      break;
+    case LAUNCH:
+      flightCmdWrapper->flight_launch();
+      break;
+    case LAND:
+      flightCmdWrapper->flight_land();
+      break;
+    case ADVANCE:
+      flightCmdWrapper->flight_advance( 0.2 );
+      break;
+    case STRIDE:
+      flightCmdWrapper->flight_stride( 0.2 );
+      break;
+    case UP:
+      flightCmdWrapper->flight_up( 0.2 );
+      break;
+    case TURN:
+      flightCmdWrapper->flight_turn( -0.2 );
+      break;
+    case HOVER:
+      flightCmdWrapper->flight_hover();
+      break;
+    case EXIT:
+      ROS_INFO( "Exiting programming" );
+      programExit = true;
+      break;
+    default:
+      ROS_INFO( "Wrong CMD" );
+      break;
+  }
+
+  return programExit;
+}
+
+// return true if flight is done
+bool flight_auto( FlightCmdWrapper *flightCmdWrapper )
+{
+  bool   programExit = false;
+  return programExit;
 }
 
