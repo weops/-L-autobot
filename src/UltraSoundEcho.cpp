1760118762d294d32b113dff7637071976f60c65
gengarbyte <gengar.byte@gmail.com>
2014-11-16 03:22:07 -0500

    splitted better

--- src/UltraSoundEcho.cpp
+++ src/UltraSoundEcho.cpp
@@ -1,4 +1,4 @@
-#include <we_autobot/UltrasonicSensor.h>
+#include <we_autobot/UltraSoundEcho.h>
 #include <time.h>
 
 //int testGPIO(int inpin, int outpin){
@@ -6,9 +6,9 @@ int main(int argc, char **argv){
   ros::init( argc, argv, "usEcho" );
   ros::NodeHandle n;
 
-  string out = "4"; //led
-  GPIOClass* gpioOut = new GPIOClass(out); //create new GPIO object to be attached to  GPIO4
-  gpioOut->export_gpio(); //export GPIO4
+  // string out = "4"; //led
+  // GPIOClass* gpioOut = new GPIOClass(out); //create new GPIO object to be attached to  GPIO4
+  // gpioOut->export_gpio(); //export GPIO4
 
   string echo = "27"; //echo
   string ussate = "1";
@@ -17,12 +17,9 @@ int main(int argc, char **argv){
   
   cout << " GPIO pins exported" << endl;
 
-  gpioOut->setdir_gpio("out"); //GPIO22 set to output
+  // gpioOut->setdir_gpio("out"); //GPIO22 set to output
   gpioEcho->setdir_gpio("in"); // set to read echo
   cout << " Set GPIO pin directions" << endl;
-
-  bool kill = 1;
-
 
   time_t pulse_start;
   time_t pulse_end;
@@ -31,17 +28,17 @@ int main(int argc, char **argv){
     time(&pulse_start);
     gpioEcho->getval_gpio(usstate);
   }
-  gpioOut->setval_gpio("1"); // turn LED ON
+  // gpioOut->setval_gpio("1"); // turn LED ON
   cout << " >> Receiving Echo : "<< usstate << endl;
   while (usstate == "1"){
     time(&pulse_end);
     gpioEcho->getval_gpio(usstate);
   }
-  gpioOut->setval_gpio("0"); // turn LED OFF
+  // gpioOut->setval_gpio("0"); // turn LED OFF
   cout << "Distance is "<< (pulse_end - pulse_start) * 17150 << endl;
 
   cout << "Exiting....." << endl;
-  gpioOut->unexport_gpio();
+  // gpioOut->unexport_gpio();
   gpioEcho->unexport_gpio();
-  return kill;
+  return 0;
 }
--- src/UltraSoundTrigger.cpp
+++ src/UltraSoundTrigger.cpp
@@ -1,4 +1,4 @@
-#include <we_autobot/UltrasonicSensor.h>
+#include <we_autobot/UltraSoungTrigger.h>
 #include <time.h>
 
 //int testGPIO(int inpin, int outpin){
@@ -11,6 +11,11 @@ int main(int argc, char **argv){
   GPIOClass* gpioIn = new GPIOClass(in); //create new GPIO object to be attached to  GPIO22
   gpioIn->export_gpio(); //export GPIO22
 
+
+  string out = "4"; //led
+  GPIOClass* gpioOut = new GPIOClass(out); //create new GPIO object to be attached to  GPIO4
+  gpioOut->export_gpio(); //export GPIO4
+
   string trig = "17"; //trigger
   GPIOClass* gpioTrig = new GPIOClass(trig); //create new GPIO object to be attached to  GPIO17
   gpioTrig->export_gpio(); //export GPIO17
@@ -18,14 +23,14 @@ int main(int argc, char **argv){
   cout << " GPIO pins exported" << endl;
 
   gpioIn->setdir_gpio("in"); // GPIO27 set to input
+  gpioOut->setdir_gpio("out"); //GPIO22 set to output
   gpioTrig->setdir_gpio("out"); //set to output trigger
   cout << " Set GPIO pin directions" << endl;
 
-  bool kill = 1;
-  while(kill){
+  bool kill = 10;
+  while(kill > 0){
     usleep(500000);  // wait for 0.5 seconds
     gpioIn->getval_gpio(inputstate); //read state of GPIO22 input pin
-    gpioEcho->getval_gpio(usstate); //read state of ultrasensor
     cout << "Current input pin state is " << inputstate  <<endl;
     if(inputstate == "0"){ // if input pin is at state "0" i.e. button pressed
       cout << "input pin state is \"Pressed \". Will check input pin state again in 20ms "<<endl;
@@ -33,6 +38,7 @@ int main(int argc, char **argv){
       cout << "Checking again ....." << endl;
       gpioIn->getval_gpio(inputstate); // checking again to ensure that state "0" is due to button press and not noise
       if(inputstate == "0"){
+        gpioOut->setval_gpio("1"); // turn LED ON
         cout << " Waiting until pin is unpressed....." << endl;
         while (inputstate == "0"){ 
           
@@ -47,21 +53,22 @@ int main(int argc, char **argv){
           usleep(10);
           gpioTrig->setval_gpio("0");
 
-
+          usleep(20000);
           gpioIn->getval_gpio(inputstate);
           // WHILE HOLD, TEST ULTRA SOUND
         };
         cout << "pin is unpressed" << endl;
-        kill = 0;
+        gpioOut->setval_gpio("0"); // turn LED OFF
+        kill = kill - 1;
       }
       else{
           cout << "input pin state is definitely \"UnPressed\". That was just noise." <<endl;
       }
     }
   }
-
   cout << "Exiting....." << endl;
   gpioIn->unexport_gpio();
+  gpioOut->unexport_gpio();
   gpioTrig->unexport_gpio();
   return kill;
 }
